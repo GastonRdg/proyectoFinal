22cfb2980650c364c93989ad5bf71993a1a40a84
Gaston Rodriguez <gaasrodriguez42@hotmail.com>
2022-10-04 22:28:29 -0300

    se agrega el proyecto

---
+++ index.js
@@ -0,0 +1,86 @@
+let nombre = prompt("Ingrese su nombre")
+function saludo() {
+
+    alert("hola bienvenido " + nombre)
+
+}
+saludo();
+
+const portaNotebook = 2500;
+const mesa = 5000;
+const biblioteca = 8000;
+menu = "";
+
+
+
+
+
+    function ivaA(peso){
+        let impuesto = ((portaNotebook*21)+portaNotebook);
+        return impuesto;
+    }
+ function ivaB(peso){
+    let impuesto = ((mesa*21)+ mesa);
+    return impuesto
+ }
+ function ivaC(peso){
+    let impuesto = ((biblioteca*21)+ biblioteca);
+    return impuesto
+ }
+ let = productoElegido = prompt("seleccione el producto que desea consultar \n1 Porta notebook \n2 mesa \n3 biblioteca \n s para salir ")
+ while (productoElegido == "s") {
+    alert("Hasta luego");
+}
+
+ while (menu != "s"){
+switch (productoElegido){
+
+    case "1":
+        alert("Elegiste Porta notebook que tiene un costo de "+portaNotebook);
+        let compra1 = prompt("ingresa 5 para confirmar compra")
+        if(compra1 == "5"){
+            let total1 = ivaA(portaNotebook);
+            alert("lo que debes abonar es $" + total1);
+        }
+        else{
+            alert("error")
+
+        }
+        break;
+        case "2":
+        alert("Elegiste Mesa que tiene un costo de "+mesa);
+        let compra2 = prompt("ingresa 5 para confirmar compra")
+        if(compra2 == "5"){
+            let total2 = ivaB(mesa);
+            alert("lo que debes abonar es $" + total2);
+        }
+        else{
+            alert("error")
+
+        }
+        break;
+
+        case "3":
+        alert("Elegiste biblioteca que tiene un costo de "+biblioteca);
+        let compra3 = prompt("ingresa 5 para confirmar compra")
+        if(compra1 == "5"){
+            let total3 = ivaC(biblioteca);
+            alert("lo que debes abonar es $" + total3);
+        }
+        else{
+            alert("error")
+
+        }
+        break;
+
+        default:
+            alert("Producto sin stock")
+            break;
+        
+}
+let = productoElegido = prompt("seleccione el producto que desea consultar \n1 Porta notebook \n2 mesa \n3 biblioteca \n s para salir ")
+
+
+
+
+}
